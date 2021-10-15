df567ab1099c2fda92a652cae391ee8d4c27b2e5
j-jurado <73544267+j-jurado@users.noreply.github.com>
2021-10-14 20:55:42 -0400

    Clean up

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -16,8 +16,6 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func onSubmitButton(_ sender: Any) {
@@ -66,15 +64,4 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
         
         dismiss(animated: true, completion: nil)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -109,7 +109,6 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         if indexPath.row == 0{
             let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
 
-
             let user = post["author"] as! PFUser
 
             cell.usernameLabel.text = user.username
@@ -153,15 +152,6 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         }
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     @IBAction func onLogoutButton(_ sender: Any) {
         PFUser.logOut()
         
